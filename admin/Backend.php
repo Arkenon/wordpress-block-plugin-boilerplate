d1dc0b68ec19b803f626b53f9cbee4dde3691ad8
Kadim Gültekin <35685546+Arkenon@users.noreply.github.com>
2023-06-13 14:18:12 +0300

    Update Backend.php

--- admin/Backend.php
+++ admin/Backend.php
@@ -5,6 +5,13 @@
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
+ * An instance of this class should be passed to the run_plugin() function
+ * defined in Loader as all of the hooks are defined
+ * in that particular class.
+ *
+ * The Loader will then create the relationship
+ * between the defined hooks and the functions defined in this class.
+ *
  * @since      1.0.0
  * @package    Plugin_Name
  * @subpackage Plugin_Name/admin
@@ -23,22 +30,21 @@ class Backend {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run_plugin() function
-		 * defined in Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( PLUGIN_NAME_TEXT_DOMAIN, plugin_dir_url( __FILE__ ) . 'css/admin.css', array(), PLUGIN_NAME_VERSION, 'all' );
 
 	}
+	
+	/**
+	 * Register the stylesheets for the block editor (Common styles).
+	 *
+	 * @since    1.0.0
+	 */
+	public function editor_styles() {
 
+		add_editor_style( array( plugin_dir_url( __FILE__ ) . 'css/editor.css' ) );
+
+	}
+	
 	/**
 	 * Register the JavaScript for the admin area.
 	 *
@@ -46,17 +52,7 @@ class Backend {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run_plugin() function
-		 * defined in Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		
 
 		wp_enqueue_script( PLUGIN_NAME_TEXT_DOMAIN, plugin_dir_url( __FILE__ ) . 'js/admin.js', array( 'jquery' ), PLUGIN_NAME_VERSION, false );
 
