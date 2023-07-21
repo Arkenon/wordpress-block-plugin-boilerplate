b3120b263c4eaee627bc91e0bf8a372e2b92b2de
Kadim Gültekin <35685546+Arkenon@users.noreply.github.com>
2023-07-21 10:32:58 +0300

    Create TranslatableText.php

---
+++ inc/I18n/TranslatableText.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This class includes a translatable text model.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/inc/I18n
+ */
+
+namespace PLUGIN_NAME;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) or die;
+
+class TranslatableText {
+
+	public $text;
+	public $context;
+
+}  
